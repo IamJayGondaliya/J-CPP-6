55a062be4f499bdbeb535bb7639d4e94f983a7ad
IamJayGondaliya <prof.rnwjay2205@gmail.com>
2024-02-05 18:46:55 +0530

    Ch-6: Inaheritence

---
+++ Ch-6/6-1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+/*
+	What inheritence?
+	- to share.
+	- Share data from one class to another class.
+	- In this concept, the class which is sharing the data
+	  will be called 'Parent class'.
+	- The class which is getting data from another class
+	  will be called 'Child class'.
+	- Similar words:
+		- Parent class		=>		Super class		=>		Base class
+		- Child class		=>		Sub class		=>		Derived class
+	
+*/
+
